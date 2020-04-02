9f53681b8d29c74344beaac0175fb7eacad566be
GreySou1 <prudvi.kamtam16@gmail.com>
2020-04-02 14:54:25 +0530

    Changed setup.py. Now it compiles

--- cython/setup.py
+++ cython/setup.py
@@ -1,6 +1,30 @@
-
+'''
 from distutils.core import setup
 from Cython.Build import cythonize
 import numpy as np
 setup(ext_modules = cythonize('usetest.pyx'),
         include_dirs = [np.get_include()])
+
+'''
+from Cython.Distutils import build_ext
+from distutils.core import setup
+from distutils.extension import Extension
+
+import os
+os.environ['CC'] = 'g++'
+os.environ['CXX'] = 'g++'
+
+ext_modules = [
+    Extension(
+        name="TestingUSECython",
+        sources=["usetest.pyx"],
+        extra_compile_args=["-Wno-cpp", "-Wno-unused-function", "-O2", "-march=native", '-stdlib=libc++', '-std=c++11'],
+        extra_link_args=["-O2", "-march=native", '-stdlib=libc++'],
+        language="c++",
+        include_dirs=["."],
+    )
+]
+
+setup(
+    name="TestingUSECython", ext_modules=ext_modules, cmdclass={"build_ext": build_ext}
+)
