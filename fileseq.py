9a3f98593eeb8fde82aceb7326d3da603482477c
GreySou1 <prudvi.kamtam16@gmail.com>
2020-04-13 14:22:26 +0530

    file sequence script

--- fileseq.py
+++ fileseq.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
+from django.utils.encoding import smart_str
 dire = 'data_set'
-fileNames = os.listdir(str(dire))
+fileNames = (os.listdir(str(dire)))
 with open('fileseq.txt', 'w') as filehandle:
-    filehandle.writelines("%s\n" % r for r in fileNames)
+    filehandle.writelines("%s\n" % smart_str(r) for r in fileNames)
